8b48db15b5c801b1a5a06a1cf8fea2b139276b73
kigawa <kigawa.8390@gmail.com>
2022-03-23 17:17:45 +0900

    update

--- src/main/java/net/kigawa/spigot/pluginutil/event/Event.java
+++ src/main/java/net/kigawa/spigot/pluginutil/event/Event.java
@@ -3,11 +3,11 @@ package net.kigawa.spigot.pluginutil.event;
 import net.kigawa.spigot.pluginutil.PluginBase;
 import org.bukkit.event.Listener;
 
-public class Event implements Listener {
+public abstract class Event implements Listener {
     PluginBase plugin;
 
-    public Event(PluginBase kigawaPlugin) {
-        plugin = kigawaPlugin;
+    public Event(PluginBase pluginBase) {
+        plugin = pluginBase;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 }
